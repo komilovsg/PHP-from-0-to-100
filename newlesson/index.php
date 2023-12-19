9bc0224ff3cbdc03eda777b35565daf6bbf4ace7
komilovsg <komilovsg@gmail.com>
2023-12-19 16:53:31 +0500

    fix new lesson

--- newlesson/index.php
+++ newlesson/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="" method="post">
         username: <br>
@@ -16,25 +18,41 @@
         <input type="submit" name="login" value="login">
     </form>
 </body>
+
 </html>
 
 <?php
-if(isset($_POST["login"])){
+if (isset($_POST["login"])) {
 
 
-    
-    $username = filter_input(INPUT_POST, "username",
-                                FILTER_SANITIZE_SPECIAL_CHARS);
-    
-    $age = filter_input(INPUT_POST, "age",
-                                FILTER_SANITIZE_NUMBER_INT);
 
-    $email = filter_input(INPUT_POST, "email",
-                                FILTER_SANITIZE_EMAIL);
+    // $username = filter_input(INPUT_POST, "username",
+    //                             FILTER_SANITIZE_SPECIAL_CHARS);
 
-    echo "Your email: {$email} is true";
-// TIME 2:50:52
+    // $age = filter_input(INPUT_POST, "age",
+    //                             FILTER_SANITIZE_NUMBER_INT);
 
+    // $email = filter_input(INPUT_POST, "email",
+    //                             FILTER_SANITIZE_EMAIL);
+
+    // echo "Your email: {$email} is true";
+    // TIME 2:50:52
+
+
+    $age = filter_input(INPUT_POST,"age",
+                        FILTER_VALIDATE_INT); 
+                        // эта функция принимает только числа, если ввести буквы, символы,
+                        // автоматикали не примет, выводит логическое сообщение
+
+    $email = filter_input(INPUT_POST,"email",
+                        FILTER_VALIDATE_EMAIL); 
+
+                if(empty($email)){
+                    echo "That email wasn`t valid";
+                } else {
+                    echo "Your email is: {$email} <br>"; // it`s work ---- два варианта написания
+                    echo "Your email is: $email"; // it`s work ---- два варианта написания
+                }
 }
 
 ?>
