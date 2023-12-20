ab3ce026bbe7f5ce80d9982cdae89ba584bf2847
komilovsg <komilovsg@gmail.com>
2023-12-20 17:05:02 +0500

    fix

---
+++ include-func/index.php
@@ -0,0 +1,15 @@
+<?php
+echo  "hello";
+
+
+    //include() = Copies the content of a file (php/html/text)
+    //            and ecludes it in your php file.
+    //            Sections of our website become reusable
+    //            Changes only need to be made in one place
+
+    // TIME TODO 2:52:37 (we stoped here)
+
+
+
+
+?>
